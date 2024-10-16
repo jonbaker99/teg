ee8a4a98b83f4afdba3c3b20307df5a6b0965004
jonbaker99 <jonbaker99@hotmail.com>
2024-10-16 23:47:56 +0100

    home update

--- streamlit/TEG_Home.py
+++ streamlit/TEG_Home.py
@@ -12,24 +12,24 @@ st.write("# The El Golfo stats & records")
 st.markdown(
     """
     ### Contents (use sidebar)
-    **1. TEG History**
+    1. **TEG History**
         - Winners by year
         - Results tables by TEG
         - Race for the title charts
     
-    2. TEG Records
+    2. **TEG Records**
         - Best TEGs, Rounds and 9s
         - Top n TEGs and Rounds
         - Personal Bests
         - Other Records
     
-    3. TEG Scoring
+    3. ### TEG Scoring
         - Score by Par
         - Career Eagles, Birdies and Pars
         - Most birdies etc. in a round
         - Best Streaks
     
-    4. Current TEG info
+    4. **Current TEG info**
         - Leaderboard
         - Latest round
         - Data update
