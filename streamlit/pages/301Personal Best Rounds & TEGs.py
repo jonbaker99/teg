aa4d91c4520a768086c632483a71df7fc05ac89c
jonbaker99 <jonbaker99@hotmail.com>
2024-10-17 00:02:28 +0100

    better pb

--- streamlit/pages/301Personal Best Rounds & TEGs.py
+++ streamlit/pages/301Personal Best Rounds & TEGs.py
@@ -2,6 +2,8 @@ from utils import load_all_data, get_best, get_ranked_teg_data, get_ranked_round
 import streamlit as st
 import numpy as np, pandas as pd
 
+st.title('Personal Best TEGs and Rounds')
+
 teg_data_ranked = get_ranked_teg_data()
 rd_data_ranked = get_ranked_round_data()
 rd_data_ranked['Round'] = rd_data_ranked['TEG'] +'|R' + rd_data_ranked['Round'].astype(str)
@@ -19,31 +21,31 @@ inverted_name_mapping = {v: k for k, v in name_mapping.items()}
 
 # Use the friendly names for the radio buttons
 friendly_names = list(name_mapping.keys())
-selected_friendly_name = st.radio("Choose a measure:", friendly_names)
+selected_friendly_name = st.radio("Choose a measure:", friendly_names, horizontal=True)
 
 # Convert to function name
 selected_measure = name_mapping.get(selected_friendly_name, selected_friendly_name)
 
-DEFAULT_TOP_N = 3
+DEFAULT_TOP_N = 1
 
-n_keep = st.number_input(
-    "Number of TEGs / Rounds to show",
-    min_value=1,
-    max_value=100,
-    value=DEFAULT_TOP_N,
-    step=1
-)
+# n_keep = st.number_input(
+#     "Number of TEGs / Rounds to show",
+#     min_value=1,
+#     max_value=100,
+#     value=DEFAULT_TOP_N,
+#     step=1
+# )
 
+n_keep = DEFAULT_TOP_N
 
-
-player_level = False
+player_level = True
 rank_measure = 'Rank_within_' + ('player' if player_level else 'all') + f'_{selected_measure}'
-
+rank_all_time = 'Rank_within_all' + f'_{selected_measure}'
 # create best teg table
 
-best_t = (get_best(teg_data_ranked,selected_measure,player_level=False, top_n = n_keep)
-          .sort_values(by=rank_measure, ascending=True)
-          .rename(columns={rank_measure: '#'})
+best_t = (get_best(teg_data_ranked,selected_measure,player_level=player_level, top_n = n_keep)
+          .sort_values(by=rank_all_time, ascending=True)
+          .rename(columns={rank_all_time: '#'})
           .rename(columns=inverted_name_mapping))
 best_t = best_t[['#','Player',selected_friendly_name,'TEG','Year']]
 
@@ -52,9 +54,9 @@ best_t[numeric_columns] = best_t[numeric_columns].astype(int)
 
 # create best round table
 
-best_r = (get_best(rd_data_ranked,selected_measure,player_level=False, top_n = n_keep)
-          .sort_values(by=rank_measure, ascending=True)
-          .rename(columns={rank_measure: '#'})
+best_r = (get_best(rd_data_ranked,selected_measure,player_level=player_level, top_n = n_keep)
+          .sort_values(by=rank_all_time, ascending=True)
+          .rename(columns={rank_all_time: '#'})
           .rename(columns=inverted_name_mapping))
 best_r = best_r[['#','Player',selected_friendly_name,'Round','Course','Year']]
 
@@ -65,9 +67,9 @@ best_r[numeric_columns] = best_r[numeric_columns].astype(int)
 tab1, tab2 = st.tabs(["Best TEGs","Best Rounds"])
 
 with tab1:
-    st.markdown(f'### Top {n_keep} TEGs: {selected_friendly_name}')
+    st.markdown(f'### Personal Best TEGs: {selected_friendly_name}')
     st.write(best_t.to_html(escape=False, index=False, justify='left'), unsafe_allow_html=True)
 
 with tab2:
-    st.markdown(f'### Top {n_keep} Rounds: {selected_friendly_name}')
+    st.markdown(f'### Personal Best Rounds: {selected_friendly_name}')
     st.write(best_r.to_html(escape=False, index=False, justify='left'), unsafe_allow_html=True)
