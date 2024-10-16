7fec33fbcee006814094dd6a92f6e4df7b8b5282
jonbaker99 <jonbaker99@hotmail.com>
2024-10-16 23:43:11 +0100

    home update

--- streamlit/TEG_Home.py
+++ streamlit/TEG_Home.py
@@ -13,9 +13,23 @@ st.markdown(
     """
     ### Contents (use sidebar)
     1. TEG History
+        a) Winners by year
+        b) Results tables by TEG
+        c) Race for the title charts
     2. TEG Records
+        a) Best TEGs, Rounds and 9s
+        b) Top n TEGs and Rounds
+        c) Personal Bests
+        d) Other Records
     3. TEG Scoring
+        a) Score by Par
+        b) Career Eagles, Birdies and Pars
+        c) Most birdies etc. in a round
+        d) Best Streaks
     4. Current TEG info
+        a) Leaderboard
+        b) Latest round
+        c) Data update
    
 """
 )
